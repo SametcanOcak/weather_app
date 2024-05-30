3e232fbd4cfa3adc5784bcd25f956461deafac1c
SametcanOcak <ocaksametcan@gmail.com>
2024-05-30 16:47:09 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+
+  const apikey = process.env.REACT_APP_WEATHER_API_KEY;
+
+  const fetchweather = async () => {
+    if (city === "") {
+      setError("Lütfen bir şehir adı girin.");
+      return;
+    };
+
+    setError("");
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
+      );
+      const data = await response.json();
+      if (data.cod !== 200) {
+        setError(data.message);
+        setWeather(null);
+      } else {
+        setWeather(data);
+      }
+    } catch (error) {
+      setError('Bir hata oluştu, lütfen tekrar deneyin.');
+      setWeather(null);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hava Durumu Uygulaması</h1>
+      <div className="input-container">
+        <input
+         type="text"
+         value={city}
+         onChange={(e) => setCity(e.target.value)}
+         placeholder="Şehir adı giriniz"
+        />
+        <button onClick={fetchweather}>Getir</button>
+      </div>
+      {error && <p className="error">{error}</p>}
+      {weather && (
+        <div className="weather-info">
+          <h2>{weather.name}, {weather.sys.country}</h2>
+          <p>Sıcaklık: {weather.main.temp}°C</p>
+          <p>Hava Durumu: {weather.weather[0].description}</p>
+          <p>Nem: {weather.main.humidity}%</p>
+        </div>
+      )}
     </div>
   );
 }
